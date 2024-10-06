5eba62550d39166bdc6770398d4b7767ef6cac3e
huyenn965 <huyennguyen3491@gmail.com>
2024-10-06 16:41:44 +0700

    Add files via upload

---
+++ Bai1_Tuan2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bai1_tuan2;
+
+/**
+ *
+ * @author Admin
+ */
+public class Bai1_Tuan2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
